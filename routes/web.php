cf143e7ccbb17e6978a7a25ac0d854e10655dcfa
Rodperez8 <rodperez.rpd@gmail.com>
2021-05-26 13:44:16 -0500

    modifique las rutas

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
-Route::get('login', function () {
-    return view('login');
-});
-Route::get('register', function () {
-    return view('registro');
+    return view('welcome');
+    //return "Hola Mundo Laravel";
 });
