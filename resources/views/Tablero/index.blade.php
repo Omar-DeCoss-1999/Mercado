6412302ec75523a42d13742fbd812654be3d617a
Rodperez8 <rodperez.rpd@gmail.com>
2021-06-01 10:02:19 -0500

    cambio de colores en index

--- resources/views/Tablero/index.blade.php
+++ resources/views/Tablero/index.blade.php
@@ -147,7 +147,7 @@
             <div id="content">
 
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <nav style="background-color:#00838f;" class="navbar navbar-expand navbar-light topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -158,7 +158,7 @@
                     <form
                         class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Buscar por..."
+                            <input name="busqueda" type="text" class="form-control bg-light border-0 small" placeholder="Buscar por..."
                                 aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
                                 <button class="btn btn-primary" type="button">
@@ -237,7 +237,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Anónimo</span>
+                                <span class="mr-2 d-none d-lg-inline small" style="color:white">Anónimo</span>
                                 <img class="img-profile rounded-circle"
                                     src="<?php echo asset('img/undraw_profile.svg')?>">
                             </a>
