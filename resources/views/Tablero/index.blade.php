992343ebf27b7a37b56b936e14defe76915b94ef
Omar-DeCoss-1999 <ruizomar003@gmail.com>
2021-06-02 14:33:21 -0500

    Funcional - corrección

--- resources/views/Tablero/index.blade.php
+++ resources/views/Tablero/index.blade.php
@@ -50,7 +50,6 @@
             <div class="sidebar-heading">
                 Catalogo
             </div>
-<<<<<<< HEAD
             @if(Auth::user() == null)
                 @yield('desplegable')
             @else
@@ -118,18 +117,6 @@
 
                     <!-- Topbar Search -->
                     @yield('buscar')
-
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="{{ route('productos.index') }}" method="get">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Buscar por.."
-                                aria-label="Search" aria-describedby="basic-addon2" name="busqueda" value="{{$busqueda}}">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="submit">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
 
                 <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
