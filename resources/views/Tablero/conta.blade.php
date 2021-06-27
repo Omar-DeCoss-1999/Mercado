04916a17f178181ffe2d1407071cc4bdd0f7cec1
Omar-DeCoss-1999 <ruizomar003@gmail.com>
2021-06-26 20:20:42 -0500

    se agregó una nueva vista a contador

--- resources/views/Tablero/conta.blade.php
+++ resources/views/Tablero/conta.blade.php
@@ -38,7 +38,7 @@
         @endif
         @empty
         <tr align="center">
-            <td colspan="3">Sin registro</td>
+            <td colspan="5">Sin registro</td>
         </tr>
         @endforelse
     </tbody>
