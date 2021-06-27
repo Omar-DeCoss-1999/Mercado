8fb026cd45d432e69536e94b14ad70dcf6f9579c
Omar-DeCoss-1999 <ruizomar003@gmail.com>
2021-06-27 11:20:08 -0500

    se agregó una nueva vista a contador

--- resources/views/Tablero/index.blade.php
+++ resources/views/Tablero/index.blade.php
@@ -51,21 +51,7 @@
                 Catalogo
             </div>
             @if(Auth::user() == null)
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Categorías</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        @forelse ($category as $categorias)
-                        <a class="collapse-item" href="productos/{{$categorias->id}}/index">{{$categorias->nombre}}</a>
-                        @empty
-                        <a class="collapse-item" href="">Sin registro</a>
-                        @endforelse
-                    </div>
-                </div>
-            </li>
+            @yield('desplegable')
             @elseif(Auth::user()->rol == 'Supervisor')
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
